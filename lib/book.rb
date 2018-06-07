83e1afa27dd8d935d09fd2070d1dc0b5d354bd7e
gabriellapowell <soccerplayer60@icloud.com>
2018-06-07 22:19:30 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -25,7 +25,7 @@ book_1 = Book.new("Paper Towns", "John Green" )
 book_2 = Book.new("Now You See Me", "Frank Furt")
 book_3 = Book.new("Fault in Our Stars" "John Green")
 
-puts book_3.page_count
+puts book_3.author
 
 
 
