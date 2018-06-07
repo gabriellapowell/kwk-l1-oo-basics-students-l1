0a36505868468dbf3d02f5fe912d2004e7b78598
gabriellapowell <soccerplayer60@icloud.com>
2018-06-07 22:26:23 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,7 +8,7 @@ def title
   @title
 end
   
-def author = (author)
+def author= (author)
   @author = author
 end
 
@@ -16,19 +16,27 @@ def author
   @author
 end
 
-def page_count =(num)
+def page_count=(num)
   @page_count = num
 end
 
 def page_count
   @page_count
 end
+
+def genre=(genre)
+  @genre = genre
+end
+
+def genre
+  @genre
+end
 end
 
 
-book_1 = Book.new("Paper Towns", "John Green" )
-book_2 = Book.new("Now You See Me", "Frank Furt")
-book_3 = Book.new("Fault in Our Stars" "John Green")
+book_1 = Book.new("Paper Towns", "John Green", 234 )
+book_2 = Book.new("Now You See Me", "Frank Furt" 456)
+book_3 = Book.new("Fault in Our Stars" "John Green" 643)
 
 puts book_3.author
 
