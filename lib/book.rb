e1c0299a94b6345f55ab3b2e298471ee20929f0f
gabriellapowell <soccerplayer60@icloud.com>
2018-06-07 22:13:19 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,17 +9,19 @@ def title
   @title
 end
 
-
+def author
+  @author
+end
 
 
 end
 
 
-book_1 = Book.new("Paper Towns")
-book_2 = Book.new("Now You See Me")
-book_3 = Book.new("Fault in Our Stars")
+book_1 = Book.new("Paper Towns", "John Green")
+book_2 = Book.new("Now You See Me", "Frank Furt")
+book_3 = Book.new("Fault in Our Stars" "John Green")
 
-puts book_3.title
+puts book_3.author
 
 
 
