80621b2d6c9d89fb66e264ef6fbdcabd6c35258d
gabriellapowell <soccerplayer60@icloud.com>
2018-06-07 22:11:05 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+
+def initialize(title)
+@title = title
+end
+def title
+  @title
+end
+end 
+
+book_1 = Book.new("Paper Towns")
+book_2 = Book.new("Now You See Me")
+book_3 = Book.new("Fault in Our Stars")
+
+puts book_3.title
+
+
+
+
+
+
+
+
 # book.rb
 # Add you Book class here
